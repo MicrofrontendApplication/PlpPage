bbb2942564a59a6c073e1c1b2e236b2bcb3825d2
Nishant <nishant.prabhu@techdenali.com>
2025-09-09 12:30:26 +0530

    sending the event to parent application

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-
-
-import './App.css'
-import PlpPage from './components/PlpPage'
+import "./App.css";
 
 function App() {
 
+  const handleClick = () => {
+    // Dispatch custom event to parent
+    document.dispatchEvent(
+      new CustomEvent("PLP_EVENT", {
+        detail: {
+          message: "Hello from PLP child app!",
+          timestamp: Date.now(),
+        },
+      })
+    );
+  };
   return (
     <>
-      {/* <PlpPage /> */}
-
+    
       <p>This is plp page </p>
+      <button onClick={handleClick}>
+        click to send event to parent application{" "}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
