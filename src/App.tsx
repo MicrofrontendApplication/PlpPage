fb3635935284d74baaa0dd11469cbc9b26c4d521
Nishant <nishant.prabhu@techdenali.com>
2025-09-17 12:37:55 +0530

    added the laoding events

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import PlpPage from "./components/PlpPage";
 function App() {
    useEffect(() => {
     // ✅ Trigger custom event after app mount
-    const event = new CustomEvent("plploaded", {
+    const event = new CustomEvent("load", {
       detail: { message: "PLP Page Loaded Successfully" }, // you can pass any payload
     });
 
